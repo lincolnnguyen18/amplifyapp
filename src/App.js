e39c1bd7f5b63788002158f208247c3561df1d44
lincolnnguyen18 <letsmakeaworld@icloud.com>
2021-10-12 15:54:21 -0500

    new text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MY FIRST AWS WEB APP DEPLOYMENT.
         </p>
         <a
           className="App-link"
